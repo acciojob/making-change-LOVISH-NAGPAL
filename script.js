fe909f7c781cfcd10d261812f4184b641ef28a10
LOVISH-NAGPAL <lovishnagpal0001@gmail.com>
2025-09-17 20:41:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 const makeChange = (c) => {
-  // your name here
+  const obj={
+	  q:0.25,
+	  d:0.10,
+	  n:0.05,
+	  p:0.01
+  }
+let obj2={
+	q:0,
+	d:0,
+	n:0,
+	p:0
+}
+
+while(c>=0 ){
+	// console.log("new while")
+for(let ch in obj){
+	// console.log(c,ch)
+	
+	if(c>=obj[ch]){
+c=(c-obj[ch])
+		obj2[ch]+=1
+		break;
+	}
+	
+}
+}
+return obj2
 };
 
-// Do not the change the code below
+Do not the change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
